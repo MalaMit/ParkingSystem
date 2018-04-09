2713a48b5a31fe921a0ecf4f41c8393a59272ba6
MalaMit <michal.r.motyka@gmail.com>
2018-04-09 23:37:15 +0200

    initial commit

--- src/main/java/databaseModel/Admin.java
+++ src/main/java/databaseModel/Admin.java
@@ -1,6 +1,53 @@
 package databaseModel;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-public class AdminAccount {
+public class Admin {
 
+	private IntegerProperty adminID;
+	private StringProperty firstName;
+	private StringProperty login;
+	private StringProperty password;
+
+	public Admin() {
+		this.adminID = new SimpleIntegerProperty();
+		this.firstName = new SimpleStringProperty();
+		this.login = new SimpleStringProperty();
+		this.password = new SimpleStringProperty();
+	}
+
+	public IntegerProperty getAdminID() {
+		return adminID;
+	}
+
+	public void setAdminID(IntegerProperty adminID) {
+		this.adminID = adminID;
+	}
+
+	public StringProperty getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(StringProperty firstName) {
+		this.firstName = firstName;
+	}
+
+	public StringProperty getLogin() {
+		return login;
+	}
+
+	public void setLogin(StringProperty login) {
+		this.login = login;
+	}
+
+	public StringProperty getPassword() {
+		return password;
+	}
+
+	public void setPassword(StringProperty password) {
+		this.password = password;
+	}
 }
