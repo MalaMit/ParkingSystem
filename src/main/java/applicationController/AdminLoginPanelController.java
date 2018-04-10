fadce0b5e1330188830a06f068c9e0f45bfec26a
MalaMit <michal.r.motyka@gmail.com>
2018-04-10 15:06:39 +0200

    loginAdmin

--- src/main/java/applicationController/AdminLoginPanelController.java
+++ src/main/java/applicationController/AdminLoginPanelController.java
@@ -4,28 +4,44 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import databaseDAOImpl.AdminDAOImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.*;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AdminLoginPanelController implements Initializable {
 
 	@FXML
+	private TextField textLogin;
+
+	@FXML
+	private PasswordField textPassword;
+
+	private AdminDAOImpl loginAdm = new AdminDAOImpl();
+
+	@FXML
 	void accessAdminButton(ActionEvent event) {
-		Parent parent = null;
-		try {
-			parent = FXMLLoader.load(getClass().getResource("/fxml/AdminPanelAccess.fxml"));
-			Scene scene = new Scene(parent);
 
-			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		if ((loginAdm.getAdminByLoginAndPassword(textLogin.getText(), textPassword.getText())) == true) {
+			Parent parent = null;
+			try {
+				parent = FXMLLoader.load(getClass().getResource("/fxml/AdminPanelAccess.fxml"));
+				Scene scene = new Scene(parent);
 
-			window.setScene(scene);
-			window.show();
+				Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				window.setScene(scene);
+				window.show();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Invalid username or password!");
 		}
 	}
 
