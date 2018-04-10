4c1fe9f3ab320dd8f77a058eb3056fe72bce2bf9
MalaMit <michal.r.motyka@gmail.com>
2018-04-10 15:06:49 +0200

    loginAdmin

--- src/main/java/databaseDAO/AdminDAO.java
+++ src/main/java/databaseDAO/AdminDAO.java
@@ -7,7 +7,7 @@ import javafx.collections.ObservableList;
 public interface AdminDAO {
 	Admin getAdmin();
 	ObservableList<Admin> getAllAdmin();
-	Admin getAdminByLoginAndPassword();
+	boolean getAdminByLoginAndPassword(String login, String password);
 	
     boolean insertAdmin();
     boolean updateAdmin();
