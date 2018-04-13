4b5c2e3b44001b7dd39d3add00dcb00d062900fd
MalaMit <michal.r.motyka@gmail.com>
2018-04-13 19:22:40 +0200

    initial commit

--- src/main/java/applicationController/CarTypeAndSpaceController.java
+++ src/main/java/applicationController/CarTypeAndSpaceController.java
@@ -1,25 +1,36 @@
 package applicationController;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXComboBox;
+
+import databaseDAOImpl.TypeVehicleDAOImpl;
+import databaseModel.TypeVehicle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
 
-
 public class CarTypeAndSpaceController implements Initializable {
-	
+
 	private UserPanelController userPanelController;
-   
+	private TypeVehicleDAOImpl typeVehicleDAOImpl = new TypeVehicleDAOImpl();
+
 	@FXML
-    void nextPage(ActionEvent event) {
-    	userPanelController.setTimeParkingPanel();
-    }
+	private JFXComboBox<TypeVehicle> typeVehicleCBox;
+
+	@FXML
+	void nextPage(ActionEvent event) {
+		userPanelController.personalDatePanel();;
+	}
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+		////Metoda toString potem może zrobie konwerter
+		ObservableList<TypeVehicle> typeV = typeVehicleDAOImpl.getAllTypeVehicle();	
+		typeVehicleCBox.getItems().addAll(typeV);
 	}
-	
+
 	public void setUserPanelController(UserPanelController userPanelController) {
 		this.userPanelController = userPanelController;
 	}
