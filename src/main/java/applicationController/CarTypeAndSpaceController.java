ca235ad7b3765feb7c675efef883ca64acf4deae
MalaMit <michal.r.motyka@gmail.com>
2018-04-17 00:07:07 +0200

    initial commit

--- src/main/java/applicationController/CarTypeAndSpaceController.java
+++ src/main/java/applicationController/CarTypeAndSpaceController.java
@@ -1,12 +1,13 @@
 package applicationController;
 
 import java.net.URL;
-
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXComboBox;
-
+import databaseDAOImpl.ParkingSpotDAOImpl;
 import databaseDAOImpl.TypeVehicleDAOImpl;
+import databaseModel.ParkingSpot;
 import databaseModel.TypeVehicle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,6 +16,7 @@ import javafx.fxml.*;
 public class CarTypeAndSpaceController implements Initializable {
 
 	private UserPanelController userPanelController;
+	private ParkingSpotDAOImpl parkingSpotDAOImpl = new ParkingSpotDAOImpl();
 	private TypeVehicleDAOImpl typeVehicleDAOImpl = new TypeVehicleDAOImpl();
 
 	@FXML
@@ -27,9 +29,15 @@ public class CarTypeAndSpaceController implements Initializable {
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		////Metoda toString potem może zrobie konwerter
-		ObservableList<TypeVehicle> typeV = typeVehicleDAOImpl.getAllTypeVehicle();	
+		ArrayList<TypeVehicle> typeV = typeVehicleDAOImpl.getAllTypeVehicle();	
 		typeVehicleCBox.getItems().addAll(typeV);
+		
+		
+		String a = "Motocykl";
+		ObservableList<ParkingSpot> parkingS = parkingSpotDAOImpl.getAllParkingSpot(a);
+		System.out.println(parkingS);
 	}
+	
 
 	public void setUserPanelController(UserPanelController userPanelController) {
 		this.userPanelController = userPanelController;
