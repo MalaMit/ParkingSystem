558eb564d8691cac6815ee3fb5acbddc3691b52b
MalaMit <michal.r.motyka@gmail.com>
2018-04-17 00:08:44 +0200

    initial commit

---
+++ src/main/java/databaseImplementDao/AdminImplementDAO.java
@@ -0,0 +1,46 @@
+package databaseImplementDao;
+
+import databaseDAO.AdminDAO;
+import databaseModel.Admin;
+import javafx.collections.ObservableList;
+import databaseUtil.DBUtil;
+
+public class AdminImplementDAO implements AdminDAO {
+
+	@Override
+	public Admin getAdmin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ObservableList<Admin> getAllAdmin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Admin getAdminByLoginAndPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertAdmin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateAdmin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAdmin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
