386b2cc8240513ed82562d0cbdf39a709277bbd5
MalaMit <michal.r.motyka@gmail.com>
2018-04-25 20:48:44 +0200

    loginAdmin

--- src/main/java/databaseDAO/ParkingSpotDAO.java
+++ src/main/java/databaseDAO/ParkingSpotDAO.java
@@ -7,8 +7,10 @@ public interface ParkingSpotDAO {
 	
 	ObservableList <ParkingSpot> getAllParkingSpot(String typ);
 	void changeStatusSpot( String numberSpot);
-
+	void changeStatusSpotExit(String numberSpot);
+	
     boolean insertParkingSpot();
     boolean updateParkingSpot();
     boolean deleteParkingSpot();
+
 }
--- src/main/java/databaseDAO/ParkingTimeDAO.java
+++ src/main/java/databaseDAO/ParkingTimeDAO.java
@@ -1,14 +1,18 @@
 package databaseDAO;
 
+
 import databaseModel.ParkingTime;
 import javafx.collections.ObservableList;
 
 public interface ParkingTimeDAO {
 	
 	ObservableList<ParkingTime> getAllTakenSPot();
+	ObservableList<ParkingTime> getToExitParking(String licensePlate);
+	boolean checkLicensePlateExist(String licensePlate);
+	int getOverrunTimePrice(String licensePlate);
 	
 	void insertParkingTime(String clientLicensePlate, String timeOut, int bill, String typeVehicle, String parkingNumber);
 	boolean deleteParkingTime();
-	boolean updateParkingTime();
+	boolean updateBillForParkingTime();
 
 }
